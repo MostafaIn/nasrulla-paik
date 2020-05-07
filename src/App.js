4e47bfca287ab30ad7a363adf57526cb89d775fd
mostafaIn <mostafa2015se@gmail.com>
2020-05-08 01:44:45 +0300

    backscreen image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {CssBaseline } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 import backScreen from './assets/images/backScreen.jpeg'
+import backScreen2 from './assets/images/backScreen2.jpeg'
+
 import Header from './components/Header'
 import Home from './components/Home'
 import Galary from './components/Galary'
@@ -33,7 +35,7 @@ const useStyles = makeStyles( (theme) =>({
 		minWidth: '100%',
 		background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8)), url(${backScreen}) center/cover fixed no-repeat`,
 		[theme.breakpoints.down('xs')]: {
-			background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8)), url(${backScreen}) center/cover fixed no-repeat`,
+			background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8)), url(${backScreen2}) center/cover fixed no-repeat`,
 		},
   }
 }));
